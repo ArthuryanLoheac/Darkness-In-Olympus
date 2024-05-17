b5abc9aa91e01e2131e8bc3c596e17b87c295658
Jean BIZEUL <jean.bizeul@epitech.eu>
2024-05-18 01:57:46 +0200

    La lumière est fixée sur la torche et fluctue faiblement

--- DarknessAthena/Assets/Scripts/basic_torch.cs
+++ DarknessAthena/Assets/Scripts/basic_torch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class basic_torch : MonoBehaviour
 {
@@ -8,8 +9,16 @@ public class basic_torch : MonoBehaviour
     private float fuel = 100f;
     private float consumption_rate = 1f; //consumption of fuel per second, set to zero to make it infinite
     private float max_radius = 1.5f;
-    private bool state = false;
+    public bool state = false;
+    private CircleCollider2D hitbox;
+    private GameObject torch;
+    public Light2D light;
 
+    void Start()
+    {
+        torch = gameObject;
+        hitbox = torch.GetComponent<CircleCollider2D>();
+    }
     public void switch_torch_state()
     {
         state = !state;
@@ -18,10 +27,11 @@ public class basic_torch : MonoBehaviour
     {
         if (state && fuel > 0)
             fuel -= time_spent * consumption_rate;
-        this.gameObject.GetComponent<CircleCollider2D>().radius = (fuel * max_radius) / max_fuel;
+        hitbox.radius = (fuel * max_radius) / max_fuel;
     }
     void Update()
     {
         update_torch_radius(Time.deltaTime);
+        light.intensity = Mathf.Log(Mathf.PingPong(Time.time, 1) + 2f);
     }
 }
