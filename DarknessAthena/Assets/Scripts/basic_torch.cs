1a772dea0a36e3e9c3e18e7bef70c07f5177223f
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2024-05-18 09:47:07 +0200

    fix torch radius and make it prefab

--- DarknessAthena/Assets/Scripts/basic_torch.cs
+++ DarknessAthena/Assets/Scripts/basic_torch.cs
@@ -6,13 +6,13 @@ using UnityEngine.Rendering.Universal;
 public class basic_torch : MonoBehaviour
 {
     private float max_fuel = 100f;
-    private float fuel = 100f;
+    public float fuel = 100f;
     private float consumption_rate = 1f; //consumption of fuel per second, set to zero to make it infinite
     private float max_radius = 1.5f;
     public bool state = false;
     private CircleCollider2D hitbox;
     private GameObject torch;
-    public Light2D light;
+    public Light2D light_torch;
 
     void Start()
     {
@@ -27,11 +27,13 @@ public class basic_torch : MonoBehaviour
     {
         if (state && fuel > 0)
             fuel -= time_spent * consumption_rate;
-        hitbox.radius = (fuel * max_radius) / max_fuel;
+        hitbox.radius = (fuel * max_radius) / max_fuel - 0.2f;
     }
     void Update()
     {
         update_torch_radius(Time.deltaTime);
-        light.intensity = Mathf.Log(Mathf.PingPong(Time.time, 1) + 2f);
+        light_torch.intensity = Mathf.Log(Mathf.PingPong(Time.time, 1) + 2f);
+        light_torch.pointLightOuterRadius = (fuel * max_radius) / max_fuel;
+        light_torch.pointLightInnerRadius = (fuel * max_radius) / max_fuel - 0.2f;
     }
 }
