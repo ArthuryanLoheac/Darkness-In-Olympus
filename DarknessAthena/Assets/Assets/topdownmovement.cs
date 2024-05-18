af88cbec79e32bf24cc83c668a608598cce56f37
Naat <nathan.bezard@epitech.eu>
2024-05-18 14:39:32 +0200

    remove les animation

--- DarknessAthena/Assets/Assets/topdownmovement.cs
+++ DarknessAthena/Assets/Assets/topdownmovement.cs
@@ -7,7 +7,6 @@ public class topdownmovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb2d;
-    public GameObject player;
     private Vector2 moveInput;
 
     // Start is called before the first frame update
@@ -21,14 +20,6 @@ public class topdownmovement : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
-
-        PlayableDirector anim = player.GetComponent<PlayableDirector>();
-        if (moveInput.y > 0 || moveInput.x > 0){
-            anim.Play();
-        }
-        if (moveInput.y < 0 || moveInput.x < 0){
-            anim.Play();
-        }
 
         moveInput.Normalize();
 
