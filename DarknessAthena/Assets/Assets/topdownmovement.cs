6ee53bfd7a38b10c1eee1eb84d1d925a9f6bbb32
Naat <nathan.bezard@epitech.eu>
2024-05-18 14:23:36 +0200

    player animation

--- DarknessAthena/Assets/Assets/topdownmovement.cs
+++ DarknessAthena/Assets/Assets/topdownmovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class topdownmovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb2d;
+    public GameObject player;
     private Vector2 moveInput;
 
     // Start is called before the first frame update
@@ -18,23 +20,14 @@ public class topdownmovement : MonoBehaviour
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
-        if (moveInput.x > 0){
-            
-            transform.Rotate(0, 0, 1);
-            transform.Rotate(0, 0, -1);
-        }
-        if (moveInput.x < 0){
-            transform.Rotate(0, 0, -2);
-            transform.Rotate(0, 0, 1);
-        }
         moveInput.y = Input.GetAxisRaw("Vertical");
-        if (moveInput.y > 0){
-            transform.Rotate(0, 0, 2);
-            transform.Rotate(0, 0, -1);
+
+        PlayableDirector anim = player.GetComponent<PlayableDirector>();
+        if (moveInput.y > 0 || moveInput.x > 0){
+            anim.Play();
         }
-        if (moveInput.y < 0){
-            transform.Rotate(0, 0, -2);
-            transform.Rotate(0, 0, 1);
+        if (moveInput.y < 0 || moveInput.x < 0){
+            anim.Play();
         }
 
         moveInput.Normalize();
